27b7e7f64ef6e9ee0ca3e24cfad6430ad5f89f58
murthychiluka <murthychiluka@gmail.com>
2026-07-23 10:05:51 +0530

    provisioners

--- Day11-provisioners/main.tf
+++ Day11-provisioners/main.tf
@@ -91,51 +91,58 @@ resource "aws_instance" "server" {
     Name = "UbuntuServer"
   }
 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"                          # ✅ Correct for Ubuntu AMIs
-    private_key = file("~/.ssh/id_rsa")             # Path to private key
-    host        = self.public_ip
-    timeout     = "6m"
-  }
+  # connection {
+  #   type        = "ssh"
+  #   user        = "ubuntu"                          # ✅ Correct for Ubuntu AMIs
+  #   private_key = file("~/.ssh/id_rsa")             # Path to private key
+  #   host        = self.public_ip
+  #   timeout     = "6m"
+  # }
 
-  provisioner "file" {
-    source      = "file10"
-    destination = "/home/ubuntu/file10"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "touch /home/ubuntu/file200",
-      "echo 'hello from Murthy hyd' >> /home/ubuntu/file200"
-    ]
-  }
-   provisioner "local-exec" {
-    command = "touch file500" 
-    
-   
- }
- }
-# resource "null_resource" "run_script" {
-#   provisioner "remote-exec" {
-#     connection {
-#       host        = aws_instance.server.public_ip
-#       user        = "ubuntu"
-#       private_key = file("~/.ssh/id_rsa")
-#     }
-
-#     inline = [
-#       "echo 'hello from veeranareshithyd' >> /home/ubuntu/file200"
-#     ]
+#   provisioner "file" {
+#     source      = "file10"
+#     destination = "/home/ubuntu/file10"
 #   }
 
-  # triggers = {
-  #   always_run = "${timestamp()}" # Forces rerun every time
-#    }
-#   triggers = {
-#  script_hash = filemd5("script.sh") # Rerun only if script changes
-# }
-# }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "touch /home/ubuntu/file200",
+#       "echo 'hello from Murthy hyd' >> /home/ubuntu/file200"
+#     ]
+#   }
+#    provisioner "local-exec" {
+#     command = "touch file500" 
+    
+   
+#  }
+ }
+resource "null_resource" "run_script" {
+  
+    connection {
+      host        = aws_instance.server.public_ip # you can use self ip
+      user        = "ubuntu"
+      private_key = file("~/.ssh/id_rsa")
+    }
+     provisioner "file" {
+    source      = "script.sh"
+   destination = "/home/ubuntu/script.sh"
+  }
+    provisioner "remote-exec" {
+
+   inline = [
+      #"echo 'hello from Murthy hyd India' >> /home/ubuntu/file200"
+       "bash /home/ubuntu/script.sh" # Assuming dev.sh is already on the instance 
+    ]
+  }
+ 
+
+  #  triggers = {
+  #    always_run = "${timestamp()}" # Forces rerun every time
+  #  }
+  triggers = {
+ script_hash = filemd5("script.sh") # Rerun only if script changes
+}
+}
 
 
 #Solution-2 to Re-Run the Provisioner
