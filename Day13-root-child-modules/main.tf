6a7bbd20434e704b365adc5fc5952c6408dc6981
murthychiluka <murthychiluka@gmail.com>
2026-07-24 23:05:08 +0530

    root-child-module

--- Day13-root-child-modules/main.tf
+++ Day13-root-child-modules/main.tf
@@ -1,32 +1,32 @@
 module "vpc" {
   source       = "./modules/vpc"
-  cidr_block   = "10.0.0.0/16"
-  subnet_1_cidr  = "10.0.15.0/24"
-  subnet_2_cidr = "10.0.16.0/24"
-  az1           = "us-east-1a"
-  az2           = "us-east-1b"
+  cidr_block   = var.cidr_block
+  subnet_1_cidr  = var.subnet_1_cidr
+  subnet_2_cidr = var.subnet_2_cidr
+  az1           = var.az1
+  az2           = var.az2
 }
 
 module "ec2" {
-   source        = "./modules/ec2"
-   ami_id        = "ami-0cca150d127c2216f"  # Replace with valid AMI
-   instance_type = "t3.micro"
-   subnet_1_id     = module.vpc.subnet_1_id
+  source        = "./modules/ec2"
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  subnet_1_id   = module.vpc.subnet_1_id
 }
 
 module "rds" {
   source         = "./modules/rds"
   subnet_1_id      = module.vpc.subnet_1_id
   subnet_2_id      = module.vpc.subnet_2_id
-  instance_class = "db.t3.micro"
-  db_name        = "mydb"
-  db_user        = "admin"
-  db_password    = "Admin12345"
+  instance_class = var.instance_class
+  db_name        = var.db_name
+  db_user        = var.db_user
+  db_password    = var.db_password
 }
 
 module "s3" {
     source = "./modules/s3"
-    bucket = "harekrishnaterraform143143"
+    bucket = var.bucket
   
 
 }
---
+++ Day13-root-child-modules/variables.tf
@@ -0,0 +1,12 @@
+variable "cidr_block"{}
+variable "subnet_1_cidr"{}
+variable "az1"{}
+variable "az2" {}
+variable "subnet_2_cidr"{}
+variable "ami_id"{}
+variable "instance_type"{}
+variable "instance_class"{}
+variable "db_name" {}
+variable "db_user" {}
+variable "db_password"{}
+variable "bucket"{}
